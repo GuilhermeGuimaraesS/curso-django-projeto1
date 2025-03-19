c272251b99aedb7dfbc48d58a9af1cdaa6878421
Guilherme Guimarães dos Santos <guilhermedossantos301@gmail.com>
2025-03-19 19:38:57 -0300

    Removendo contato e sobre.

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Home
-    path('sobre/', views.sobre, name='sobre'),  # Sobre
-    path('contato/', views.contato, name='contato'),  # Contato
 ]
